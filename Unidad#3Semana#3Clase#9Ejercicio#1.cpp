7e700b11497fca19b7bf659f8fceb786c4fd2f40
IselaBrizuela <113409059+IselaBrizuela@users.noreply.github.com>
2022-09-13 00:44:06 -0600

    Add files via upload

---
+++ Unidad#3Semana#3Clase#9Ejercicio#1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int numero;
+	do
+	{
+		cout << " Ingres un numero";
+		cin >> numero;
+	}
+	while(numero <= 100);
+	system(" PAUSE ");
+	return 0;
+}
